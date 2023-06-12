7ecf899c610832f673ff862bf67b56d59311f612
Edgar de Souza Vismara <edgarvismara@utfpr.edu.br>
2023-06-12 15:48:33 -0300

    Update 02_MLExperimentsMLR.R

--- code/02_MLExperimentsMLR.R
+++ code/02_MLExperimentsMLR.R
@@ -46,9 +46,6 @@ if(!dir.exists("~/capsule/results/deepLearningResults")) {
   dir.create("~/capsule/results/deepLearningResults", showWarnings=FALSE, recursive=TRUE)
 }
 
-if(!dir.exists("~/capsule/data/dataset")) {
-  dir.create("~/capsule/data/dataset", showWarnings=FALSE, recursive=TRUE)
-}
 
 ## ------------------------------------------------------------------------------------------
 ## 2. Preprocessing data
